c66101ee503ad2e43d3ae04229275c62ac337446
NucciTheBoss <jason.nucciarone@canonical.com>
2022-09-19 17:37:32 -0400

    Add status messages

--- src/charm.py
+++ src/charm.py
@@ -6,6 +6,7 @@
 
 import logging
 
+from hpctlib.misc import service_forced_update
 from hpctlib.ops.charm.service import ServiceCharm
 from ops.charm import ActionEvent, InstallEvent, StartEvent, StopEvent
 from ops.framework import StoredState
@@ -31,24 +32,45 @@ class SlurmClientCharm(ServiceCharm):
 
     def _auth_start(self, event: ActionEvent) -> None:
         """Fired when auth-start is run."""
+        self.service_set_state("idle")
+        self.service_set_status_message("Starting munge")
         self.auth_manager.start()
+        self.service_set_state("started")
+        self.service_set_status_message()
 
     def _auth_stop(self, event: ActionEvent) -> None:
         """Fired when auth-stop is run."""
+        self.service_set_state("idle")
+        self.service_set_status_message("Stopping munge")
         self.auth_manager.stop()
+        self.service_set_state("broken")
+        self.service_set_status_message("Authentication service is not active.")
 
     def _service_install(self, event: InstallEvent) -> None:
         "Fired when charm is first deployed."
+        self.service_set_state("waiting")
+        self.service_set_status_message("Installing munge")
         self.auth_manager.install()
+        self.service_set_status_message("Installing slurmd")
         self.manager.install()
+        self.service_set_state("started")
+        self.service_set_status_message()
 
     def _service_start(self, event: StartEvent) -> None:
         """Fired when service-start is run."""
+        self.service_set_state("idle")
+        self.service_set_status_message("Starting slurmd")
         self.manager.start()
+        self.service_set_state("started")
+        self.service_set_status_message()
 
     def _service_stop(self, event: StopEvent, force: bool) -> None:
         """Fired when service-stop is run."""
+        self.service_set_state("idle")
+        self.service_set_status_message("Stopping slurmctld")
         self.manager.stop()
+        self.service_set_state("broken")
+        self.service_set_status_message("Slurm server is not active.")
 
 
 if __name__ == "__main__":
