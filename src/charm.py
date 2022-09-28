94c60c079e9117425ff720d0cbc61d499735f795
NucciTheBoss <jason.nucciarone@canonical.com>
2022-09-28 11:09:12 -0400

    Boilerplate for relation interfaces

--- src/charm.py
+++ src/charm.py
@@ -6,11 +6,17 @@
 
 import logging
 
+from hpctlib.interface import interface_registry
 from hpctlib.misc import service_forced_update
 from hpctlib.ops.charm.service import ServiceCharm
 from ops.charm import InstallEvent, StartEvent, StopEvent
 from ops.main import main
 
+from interface import (
+    AuthMungeInterface,
+    SlurmComputeInterface,
+    SlurmControllerInterface,
+)
 from manager import MungeManager, SlurmClientManager
 
 logger = logging.getLogger(__name__)
@@ -21,8 +27,19 @@ class SlurmClientCharm(ServiceCharm):
 
     def __init__(self, *args):
         super().__init__(*args)
+
         self.slurm_client_manager = SlurmClientManager()
         self.munge_manager = MungeManager()
+
+        self.auth_munge_interface = interface_registry.load(
+            "relation-auth-munge", self, "auth-munge"
+        )
+        self.slurm_compute_interface = interface_registry.load(
+            "relation-slurm-compute", self, "slurm-compute"
+        )
+        self.slurm_controller_interface = interface_registry.load(
+            "relation-slurm-controller", self, "slurm-controller"
+        )
 
     @service_forced_update()
     def _service_install(self, event: InstallEvent) -> None:
@@ -68,4 +85,7 @@ class SlurmClientCharm(ServiceCharm):
 
 
 if __name__ == "__main__":
+    interface_registry.register("relation-auth-munge", AuthMungeInterface)
+    interface_registry.register("relation-slurm-compute", SlurmComputeInterface)
+    interface_registry.register("relation-slurm-controller", SlurmControllerInterface)
     main(SlurmClientCharm)
